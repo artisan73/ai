c400d398cee4d06c0cc1d4a141a17c4d880d44a7
artisan73 <magemberonald73@gmail.com>
2025-03-02 06:08:55 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+                                        VANTA.NET({
+    el: "#animation-background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0x00a2ff,
+    backgroundColor: 0x0,
+    points: 15.0,
+    maxDistance: 25.0,
+    spacing: 17.0,
+  }); 
+            
